807e706309dcfa209aff488a7aa36b66fd394feb
Zahraahb <zahraahb55@gmail.com>
2024-05-28 16:03:29 +0400

    mysql2

--- db/connectionDB.js
+++ db/connectionDB.js
@@ -1,7 +1,16 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
 export const sequelize = new Sequelize(
-  "mysql://uwlxiibgbzetdlg0:P6WN6XOCgfaKPtliAHvy@bsfxwlyld8gs2f999zjo-mysql.services.clever-cloud.com:3306/bsfxwlyld8gs2f999zjo"
+  "bsfxwlyld8gs2f999zjo",
+  "uwlxiibgbzetdlg0",
+  "P6WN6XOCgfaKPtliAHvy",
+  {
+    host: "bsfxwlyld8gs2f999zjo-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+    dialectModule: mysql2,
+    port: 3306,
+  }
 );
 const connectionDB =async()=>{
     return await sequelize.sync({alter:false,force:false}).then(()=>{
